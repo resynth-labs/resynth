b0b1fe3001ff833c9350a397838849f4e7ad88a5
Partially Sorted <partiallysorted@gmail.com>
2023-02-25 09:57:36 -0600

    Added sdk.

---
+++ sdk/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./client";
+export * from "./idl";
+export * from "./types";
---
+++ sdk/src/types.ts
@@ -0,0 +1,44 @@
+import { BN } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+
+// Constants ----------------------------------------------------------------
+
+export const AUTHORITY: string = "authority";
+
+// Accounts -----------------------------------------------------------------
+
+export type MarginAccount = {
+  owner: PublicKey;
+  syntheticAsset: PublicKey;
+  collateralDeposited: BN;
+  syntheticAssetBorrowed: BN;
+}
+
+export type SyntheticAsset = {
+  syntheticAsset: PublicKey;
+  collateralMint: PublicKey;
+  collateralVault: PublicKey;
+  syntheticMint: PublicKey;
+  syntheticOracle: PublicKey;
+  assetAuthority: PublicKey;
+  assetAuthorityBump: number[];
+}
+
+// Errors -------------------------------------------------------------------
+
+export class ResynthError {
+  static readonly InvalidOracle = { name: "InvalidOracle", code: 6000, message: "The oracle account provided is invalid" };
+  static readonly StaleOracle = { name: "StaleOracle", code: 6001, message: "The oracle is stale" };
+  static readonly Undercollateralized = { name: "Undercollateralized", code: 6002, message: "The margin account is undercollateralized" };
+  static readonly InvalidCollateralMintDecimals = { name: "InvalidCollateralMintDecimals", code: 6003, message: "Collateral decimals is rqeuired to be 6 because it's value is hardcoded to $1" };
+
+  static fromErrorCode(errorCode: number): any {
+    switch (errorCode) {
+      case 6000: return ResynthError.InvalidOracle;
+      case 6001: return ResynthError.StaleOracle;
+      case 6002: return ResynthError.Undercollateralized;
+      case 6003: return ResynthError.InvalidCollateralMintDecimals;
+      default: return { name: "Unknown", code: errorCode };
+    }
+  }
+}
