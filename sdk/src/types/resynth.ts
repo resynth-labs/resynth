d07957d58af0c479bb2f93e6e03b5a5126975627
Partially Sorted <partiallysorted@gmail.com>
2023-02-28 07:50:10 -0600

    Updated sdk

--- sdk/src/types/resynth.ts
+++ sdk/src/types/resynth.ts
@@ -31,18 +31,40 @@ export type SyntheticAsset = {
 // Errors -------------------------------------------------------------------
 
 export class ResynthError {
-  static readonly InvalidOracle = { name: "InvalidOracle", code: 6000, message: "The oracle account provided is invalid" };
-  static readonly StaleOracle = { name: "StaleOracle", code: 6001, message: "The oracle is stale" };
-  static readonly Undercollateralized = { name: "Undercollateralized", code: 6002, message: "The margin account is undercollateralized" };
-  static readonly InvalidCollateralMintDecimals = { name: "InvalidCollateralMintDecimals", code: 6003, message: "Collateral decimals is rqeuired to be 6 because it's value is hardcoded to $1" };
+  static readonly InvalidOracle = {
+    name: "InvalidOracle",
+    code: 6000,
+    message: "The oracle account provided is invalid",
+  };
+  static readonly StaleOracle = {
+    name: "StaleOracle",
+    code: 6001,
+    message: "The oracle is stale",
+  };
+  static readonly Undercollateralized = {
+    name: "Undercollateralized",
+    code: 6002,
+    message: "The margin account is undercollateralized",
+  };
+  static readonly InvalidCollateralMintDecimals = {
+    name: "InvalidCollateralMintDecimals",
+    code: 6003,
+    message:
+      "Collateral decimals is rqeuired to be 6 because it's value is hardcoded to $1",
+  };
 
   static fromErrorCode(errorCode: number): any {
     switch (errorCode) {
-      case 6000: return ResynthError.InvalidOracle;
-      case 6001: return ResynthError.StaleOracle;
-      case 6002: return ResynthError.Undercollateralized;
-      case 6003: return ResynthError.InvalidCollateralMintDecimals;
-      default: return { name: "Unknown", code: errorCode };
+      case 6000:
+        return ResynthError.InvalidOracle;
+      case 6001:
+        return ResynthError.StaleOracle;
+      case 6002:
+        return ResynthError.Undercollateralized;
+      case 6003:
+        return ResynthError.InvalidCollateralMintDecimals;
+      default:
+        return { name: "Unknown", code: errorCode };
     }
   }
 }
